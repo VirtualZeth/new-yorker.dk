7e8cf2690ca2f94f4ceeb3243912691979b63267
Danijel <dani025a@edu.easj.dk>
2021-06-02 12:33:16 +0200

    add if statements for ContactForm

--- 08 implementation/Mobile App/app/src/main/java/com/example/newyorkerdk/usecase/MailCredentials.java
+++ 08 implementation/Mobile App/app/src/main/java/com/example/newyorkerdk/usecase/MailCredentials.java
@@ -2,7 +2,7 @@ package com.example.newyorkerdk.usecase;
 
 public class MailCredentials {
     private static final  String EMAIL = "danijelgitanovic@gmail.com";
-    private static final  String PASSWORD = "eqqmugikexvhpcec";
+    private static final  String PASSWORD = "";
     private static final String NewYorkerMail = "info@new-yorkwe.dk";
 
     public static String getEMAIL() {
