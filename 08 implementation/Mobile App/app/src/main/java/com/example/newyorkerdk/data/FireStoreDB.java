cfe11f845a64ab2e8146efe43f8a8b96f13b8942
LostButStillImproving <mike6353@edu.easj.dk>
2021-06-22 15:49:20 +0200

    Optimize imports, remove unused fields,

--- 08 implementation/Mobile App/app/src/main/java/com/example/newyorkerdk/data/FireStoreDB.java
+++ 08 implementation/Mobile App/app/src/main/java/com/example/newyorkerdk/data/FireStoreDB.java
@@ -1,11 +1,6 @@
 package com.example.newyorkerdk.data;
 
-import androidx.lifecycle.MutableLiveData;
-
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.List;
-import java.util.Map;
 
 
 public class FireStoreDB {
@@ -14,8 +9,6 @@ public class FireStoreDB {
     private static final FireStoreDB instance = new FireStoreDB();
 
     private final FirebaseFirestore database = FirebaseFirestore.getInstance();
-    private final MutableLiveData<Map<String, Double>> productPriceList = new MutableLiveData<>();
-    private final MutableLiveData<List<String>> categoriesList = new MutableLiveData<>();
 
     public FireStoreDB() {
     }
